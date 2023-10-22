ee8bf927b2cb685a8e8e582d2330e0400cc3c244
Eduardo Função <45571086+eduardofuncao@users.noreply.github.com>
2023-10-22 17:29:49 -0300

    adiciona documento .md e .pdf para relatório pesquisa e definição do projeto para entrega de java

--- entregaJava/SalesPop/src/funcionalidades/Cookies.java
+++ entregaJava/SalesPop/src/funcionalidades/Cookies.java
@@ -1,5 +1,19 @@
 package funcionalidades;
 
 public class Cookies {
+    private String id;
+    private String nome;
+    private String dominio;
+    private String dataExpira;
+    private boolean preferencias;
+    private boolean estatistica;
+    private boolean marketing;
 
+    public void recebeInfo() {
+        // Implementação do método
+    }
+
+    public void submit() {
+        // Implementação do método
+    }
 }
